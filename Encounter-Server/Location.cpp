30f40aea6b8370cb6b3ea0215543515a662cd344
KrzysiekBlankiewicz <krzysztof.blankiewicz@gmail.com>
2019-01-12 16:43:10 +0100

    ustawianie na lokacji

--- Encounter-Server/Location.cpp
+++ Encounter-Server/Location.cpp
@@ -9,16 +9,16 @@ using namespace std;
 
 
 Location::Location(string fileName, int newId): backgroundSource(fileName), id(newId) {
-	Character* mob = new Character(/* x, y, s, i, v, name*/ 1, 1, 1, 1, 1, "mob1.png");
+	Character* mob = new Character(/* x, y, s, i, v, name*/ 7, 5, 1, 1, 1, "mob1.png");
 	occupation[1][1] = mob;
 
-	Dealer* dealer = new Dealer(/* x, y, saleFactor, name*/2, 2, 0.5, "dealer1.png");
+	Dealer* dealer = new Dealer(/* x, y, saleFactor, name*/5, 3, 0.5, "dealer1.png");
 	occupation[2][2] = dealer;
 
-	Chest* chest = new Chest(/* x, y, gold, name */3, 3, 20, "chest1.png");
+	Chest* chest = new Chest(/* x, y, gold, name */4, 8, 20, "chest1.png");
 	occupation[3][3] = chest;
 
-	Nature* nature = new Nature(/* x, y, name */ 4, 4, "nature1.png");
+	Nature* nature = new Nature(/* x, y, name */ 7, 7, "nature1.png");
 	occupation[4][4] = nature;
 }
 
