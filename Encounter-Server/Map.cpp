6e79d1bd42a9d63adbc1b91fa0c7574fc70c4162
KrzysiekBlankiewicz <krzysztof.blankiewicz@gmail.com>
2019-01-12 10:57:06 +0100

    temp commit

--- Encounter-Server/Map.cpp
+++ Encounter-Server/Map.cpp
@@ -1,9 +1,16 @@
 #include "Map.h"
+#include <string>
+using namespace std;
 
 vector<Card*> Map::allCards = {};
 
 Map::Map() {
-
+    Location* location;
+    for(int i = 0; i < 4; ++i)
+    {
+        location = new Location(string sourceName);
+        locations[i] = location;
+    }
 }
 
 
